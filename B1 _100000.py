d35810793c618799e2daebf39be571c43d42b1ca
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-12-01 02:05:03 +0300

    add random integer and float numbers

--- B1 _100000.py
+++ B1 _100000.py
@@ -1,6 +1,6 @@
 # Работа выполнена Каптуровым Александром
 
-import random
+from random import randint, randrange
 import datetime
 from string import ascii_lowercase, ascii_uppercase
 
@@ -15,21 +15,35 @@ def generate_russian_alphabet():
 
 
 def get_random_alphabet_combination(alphabet):
-    phrase = [alphabet()[random.randint(0, len(alphabet()) - 1)] for _ in range(10)]
+    phrase = [alphabet()[randint(0, len(alphabet()) - 1)] for _ in range(10)]
     return "".join(phrase)
 
 
-def file_creator():
+def get_integer_even_number_combination():
+    return randrange(2, 100_000_000, 2)
+
+
+def get_float_number_combination():
+    return randint(100_000_000, 2_000_000_000) / 100_000_000
+
+
+def files_creator():
     for name in range(1, 101):
         with open(f'{name}.txt', 'w', encoding='UTF-8') as file:
             for _ in range(100_000):
                 message = list()
                 message.append(get_random_alphabet_combination(generate_alphabet))
                 message.append(get_random_alphabet_combination(generate_russian_alphabet))
-                message = '||'.join(message)
-                file.write(f'{message}\n')
+                message.append(str(get_integer_even_number_combination()))
+                message.append(str(get_float_number_combination()).replace('.', ','))
+                result = '||'.join(message)
+                file.write(f'{result}\n')
 
 
-file_creator()
+if __name__ == "__main__":
+    files_creator()
+    
+
 # print(get_random_latin_combination())
 # print(get_random_alphabet_combination(generate_russian_alphabet))
+# print(get_float_number_combination())
