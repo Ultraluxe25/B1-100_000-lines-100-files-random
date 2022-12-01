0a4b7230c193112f6f1ba62f73856d80ed807299
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-12-01 23:37:17 +0300

    OOP variant

--- second stage.py
+++ second stage.py
@@ -1,16 +1,23 @@
 # Данный код разработал Каптуров Александр для собеседования в B1 (бывыший EY)
 
-with open('second stage.txt', 'w', encoding='UTF-8') as big_file:
-    pattern = 'abc'  # U can also use pattern = input() for other patterns
-    pattern_match = 0
+class GeneralFileWithPatternRemover:
+    @staticmethod
+    def show_quantity_of_pattern_match() -> str:
+        with open('second stage.txt', 'w', encoding='UTF-8') as big_file:
+            pattern = 'abc'  # U can also use pattern = input() for other patterns
+            pattern_match = 0
 
-    for file in range(1, 101):
-        with open(f'{file}.txt', 'r', encoding='UTF-8') as current_file:
-            info = current_file.read()
+            for file in range(1, 101):
+                with open(f'{file}.txt', 'r', encoding='UTF-8') as current_file:
+                    info = current_file.read()
 
-            pattern_match += info.count(pattern)
-            info.replace(pattern, '')
+                    pattern_match += info.count(pattern)
+                    info.replace(pattern, '')
 
-            big_file.write(info)
+                    big_file.write(info)
 
-print(pattern_match)
+        return f'Найдено совпадений: {pattern_match}'
+
+
+new_file = GeneralFileWithPatternRemover()
+print(new_file.show_quantity_of_pattern_match())
