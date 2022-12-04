22097528c50901775f193c629f49bf337554ce55
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-12-04 15:50:52 +0300

    final version on 4th part

--- B1 _100000_4_part.py
+++ B1 _100000_4_part.py
@@ -6,10 +6,15 @@ Developed by Kapturov Alexander
 """
 
 import sqlite3
-import numpy as np
+from time import time
+import numpy as np  # For getting median. Use 'pip install numpy' to execute this file
 
 
-def fulfill_database():
+def create_database() -> None:
+    """
+    Function creates database
+    :return: None
+    """
     with sqlite3.connect('B1.db') as con:
         cur = con.cursor()  # In order to execute SQL statements and fetch results from SQL queries
 
@@ -25,40 +30,75 @@ def fulfill_database():
             float_number REAL
             )""")
 
-    for name in range(1, 2):
-        print(f'Записывается {name} файл')
-        with open(f'{name}.txt', 'r', encoding='UTF-8') as file:
-            data = file.readlines()
-            for line in data:
-                record = line[:-1].split('||')
-                col1, col2, col3, col4, col5 = record
-                col4 = int(col4)
-                col5 = float(col5.replace(',', '.'))
-                cur.execute(f"""INSERT INTO B1_data(calendar, english_alphabet, russian_alphabet, \
-                integer_number, float_number)
-                VALUES ('{col1}', '{col2}', '{col3}', '{col4}', '{col5}')""")
-                # # print(line[:-1].split('||'))
-                # print(col1, col2, col3, col4, col5)
-        con.commit()
+
+def fill_database_with_file(file) -> None:
+    """
+    Function fills database with data from current file
+    :param file: The current file from which the data for the database is unloaded
+    :return: None
+    """
+    with sqlite3.connect('B1.db') as con:
+        cur = con.cursor()
+        data = file.readlines()
+        for line in data:
+            record = line[:-1].split('||')
+            col1, col2, col3, col4, col5 = record
+            col4 = int(col4)
+            col5 = float(col5.replace(',', '.'))
+            cur.execute(f"""INSERT INTO B1_data(calendar, english_alphabet, russian_alphabet, \
+                            integer_number, float_number)
+                            VALUES ('{col1}', '{col2}', '{col3}', '{col4}', '{col5}')""")
 
 
-def get_int_sum(data_base='B1.db'):
+def fill_database_with_all_files(database='B1.db') -> None:
+    """
+    Function for fill database with data from all 100 files
+    :param database: Current database which is being filled
+    :return: None
+    """
+    with sqlite3.connect(database) as con:
+        for name in range(1, 101):
+            print(f'The {name} file is written to the database')
+            with open(f'{name}.txt', 'r', encoding='UTF-8') as file:
+                fill_database_with_file(file)
+            con.commit()
+
+
+def get_int_sum(data_base='B1.db') -> int:
+    """
+    Function return total sum of all integer numbers from 'integer_number' column
+    :param data_base: Database which used to get data
+    :return: Integer number's sum value
+    """
     with sqlite3.connect(data_base) as con:
         cur = con.cursor()
         for value in cur.execute('SELECT SUM(integer_number) FROM B1_data'):
             return value[0]
 
 
-def getfloat_median(data_base='B1.db'):
+def get_float_median(data_base='B1.db') -> np.array:
+    """
+    Function return float number's median value from 'float_number' column
+    :param data_base: Database which used to get data
+    :return: Float number's median value
+    """
     with sqlite3.connect(data_base) as con:
         cur = con.cursor()
         float_numbers_list = []
-        for value in cur.execute(f'SELECT float_number FROM B1_data'):
+        for value in cur.execute('SELECT float_number FROM B1_data'):
             float_numbers_list.append(value[0])
         return np.mean(float_numbers_list)
 
 
 if __name__ == "__main__":
-    fulfill_database()
-    print(f'Integer number\'s sum value is: {get_int_sum()}')
-    print(f'Float number\'s median value is: {getfloat_median()}')
+    start_time = time()
+
+    create_database()
+    fill_database_with_all_files('B1.db')
+    print(f'\nInteger number\'s sum value is: {get_int_sum()}')
+    print(f'Float number\'s median value is: {get_float_median()}')
+
+    end_time = time()
+    execution_time = end_time - start_time
+    print(f'execution_time: {execution_time:.2f} seconds')
+# pylint score is 10.00/10
